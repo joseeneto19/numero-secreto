04eb397185ebc3767a7ef9da917a1fff46c460c4
José Neto <josemnetodev@gmail.com>
2024-02-19 10:08:47 -0300

    adicionado o botao novo jogo

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ function exibirTextoNaTela(tag, texto) {
   campo.innerHTML = texto;
 }
 
-exibirTextoNaTela("h1", "Jogo do número secreto");
-exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+function exibirMensagemInicial() {
+  exibirTextoNaTela("h1", "Jogo do número secreto");
+  exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
   let chute = document.querySelector("input").value;
@@ -17,6 +21,7 @@ function verificarChute() {
     let palavraTentativa = tentativas > 1 ? "tentativas" : "tentaiva";
     let mensagemTentativas = `Você descobriu o numero secreto com ${tentativas} ${palavraTentativa}!`;
     exibirTextoNaTela("p", mensagemTentativas);
+    document.getElementById('reiniciar').removeAttribute('disabled')
   } else {
     if (chute > numeroSecreto) {
       exibirTextoNaTela("p", "O número secreto é menor");
@@ -35,4 +40,12 @@ function gerarNumeroAleatorio() {
 function limparCampo() {
   chute = document.querySelector('input');
   chute.value = '';
+}
+
+function reiniciarJogo() {
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo()
+  tentativas = 1;
+  exibirMensagemInicial();
+  document.getElementById('reiniciar').setAttribute('disabled', true);
 }
